d680d2e49d5012d49463598ec4d9e11d86eabff0
Alex-Nicalace <anicalace@gmail.com>
2023-12-19 11:13:26 +0200

    usung global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App(): JSX.Element {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Hello, World!</H1>
+        <Button>Button</Button>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
