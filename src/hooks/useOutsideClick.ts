a0c2c1599fbb7eeb619bf2477a60c19833d8e668
Alex-Nicalace <anicalace@gmail.com>
2024-11-14 14:56:15 +0200

    fix 404

--- src/hooks/useOutsideClick.ts
+++ src/hooks/useOutsideClick.ts
@@ -4,8 +4,8 @@ import { useEffect, useRef } from 'react';
  * Функция, обрабатывающая клик за пределами указанного элемента.
  *
  * @param {(e: MouseEvent) => void} handler - колбэк функция, которая будет выполнена при клике за пределами элемента
- * @param {boolean} [capture=true] - флаг для указания использования фазы захвата
  * @param {boolean} [ignore=false] - флаг для указания игнорирования клика за пределами элемента
+ * @param {boolean} [capture=true] - флаг для указания использования фазы захвата
  * @return {React.MutableRefObject<T>} ссылка на элемент
  */
 export function useOutsideClick<T extends HTMLElement>(
